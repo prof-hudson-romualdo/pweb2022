9529e4426f53eb3c74a270ebde57221f9319e979
Hudson Romualdo <hudson.paula@gmail.com>
2022-08-15 11:38:13 -0300

    Meu primeiro commit

---
+++ maven-web/src/test/java/br/edu/ifgoiano/mavenweb/MavenWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifgoiano.mavenweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
